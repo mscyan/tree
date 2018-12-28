b402470980ad7d85b5ce0c2fe7eb0ffa1f1cb068
yanchao <yanchao00001@hotmail.com>
2018-12-28 22:01:12 +0800

    LCA-二叉搜索树的最近公共祖先

--- LowestCommonAncester.h
+++ LowestCommonAncester.h
@@ -11,7 +11,12 @@ class LowestCommonAncestor
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-
-        return nullptr;
+        if(root == nullptr)
+            return nullptr;
+        if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
     }
 };
